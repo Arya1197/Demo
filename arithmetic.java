b2c78ef02357849b55922c1d49942864707a38d0
unknown <aryamuralidharan1107@gmail.com>
2021-04-21 16:46:57 -1200

    first commit

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic
+{
+    public static void main(String[]args)
+    {
+        int num1=20;
+        int num2=10;
+        int sum=num1+num2;
+        int diffrence=num1-num2;
+        int product=num1*num2;
+        int division=num1/num2;
+        System.out.println("Number1: "+num1);
+        System.out.println("Number2: "+num2);
+        System.out.println("Sum: "+sum);
+        System.out.println("Difference: "+diffrence);
+        System.out.println("Product: "+product);
+        System.out.println("Division: "+division);   
+    }
+}
